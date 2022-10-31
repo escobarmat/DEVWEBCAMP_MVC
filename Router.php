1fd15fed2afcbd905b6bcffe0110a2dabcd3059c
Escobar <escobar@matias.com>
2022-10-31 15:01:01 -0300

    Solucionado layout admin

--- Router.php
+++ Router.php
@@ -53,7 +53,11 @@ class Router
         $contenido = ob_get_clean(); // Limpia el Buffer
 
         //Utilizar el layout de acuerdo a la URL
-        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+        if (isset($_SERVER['PATH_INFO'])) {
+            $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $url_actual = strtok($_SERVER["REQUEST_URI"], '?') ?? '/';
+        }
         if(str_contains($url_actual, "/admin")){
             include_once __DIR__ . '/views/admin-layout.php'; 
         }else{
